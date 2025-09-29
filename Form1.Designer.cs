59076c6deed91c21651327eb7977bc446684d1dc
holog <holog@DANGDGM>
2025-09-29 13:05:59 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace GeminiC__App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            rtbOutput = new RichTextBox();
+            rtbRun = new RichTextBox();
+            btnGenerate = new Button();
+            btnExecute = new Button();
+            txtPrompt = new TextBox();
+            lblPerformance = new Label();
+            btnOpen = new Button();
+            SuspendLayout();
+            // 
+            // rtbOutput
+            // 
+            rtbOutput.BackColor = Color.Azure;
+            rtbOutput.Font = new Font("Montserrat Medium", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rtbOutput.ForeColor = SystemColors.InfoText;
+            rtbOutput.Location = new Point(28, 21);
+            rtbOutput.Name = "rtbOutput";
+            rtbOutput.ReadOnly = true;
+            rtbOutput.Size = new Size(762, 354);
+            rtbOutput.TabIndex = 0;
+            rtbOutput.Text = "";
+            // 
+            // rtbRun
+            // 
+            rtbRun.BackColor = Color.Azure;
+            rtbRun.Font = new Font("Montserrat Medium", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rtbRun.ForeColor = SystemColors.InfoText;
+            rtbRun.Location = new Point(813, 21);
+            rtbRun.Name = "rtbRun";
+            rtbRun.Size = new Size(292, 354);
+            rtbRun.TabIndex = 1;
+            rtbRun.Text = "";
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.BackColor = Color.MintCream;
+            btnGenerate.Cursor = Cursors.Hand;
+            btnGenerate.FlatStyle = FlatStyle.Popup;
+            btnGenerate.Font = new Font("Montserrat Medium", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnGenerate.Location = new Point(897, 396);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(134, 42);
+            btnGenerate.TabIndex = 2;
+            btnGenerate.Text = "Generate";
+            btnGenerate.UseVisualStyleBackColor = false;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // btnExecute
+            // 
+            btnExecute.BackColor = Color.MintCream;
+            btnExecute.Cursor = Cursors.Hand;
+            btnExecute.FlatStyle = FlatStyle.Popup;
+            btnExecute.Font = new Font("Montserrat Medium", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnExecute.Location = new Point(897, 444);
+            btnExecute.Name = "btnExecute";
+            btnExecute.Size = new Size(134, 42);
+            btnExecute.TabIndex = 3;
+            btnExecute.Text = "Execute";
+            btnExecute.UseVisualStyleBackColor = false;
+            btnExecute.Click += btnExecute_Click_1;
+            // 
+            // txtPrompt
+            // 
+            txtPrompt.Anchor = AnchorStyles.Left;
+            txtPrompt.BackColor = Color.MintCream;
+            txtPrompt.Cursor = Cursors.IBeam;
+            txtPrompt.Font = new Font("Montserrat Medium", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtPrompt.ForeColor = SystemColors.ControlText;
+            txtPrompt.Location = new Point(28, 396);
+            txtPrompt.Multiline = true;
+            txtPrompt.Name = "txtPrompt";
+            txtPrompt.Size = new Size(762, 61);
+            txtPrompt.TabIndex = 4;
+            txtPrompt.TextChanged += txtPrompt_TextChanged;
+            // 
+            // lblPerformance
+            // 
+            lblPerformance.AutoSize = true;
+            lblPerformance.Font = new Font("Montserrat SemiBold", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblPerformance.ForeColor = SystemColors.Desktop;
+            lblPerformance.Location = new Point(28, 477);
+            lblPerformance.Name = "lblPerformance";
+            lblPerformance.Size = new Size(61, 24);
+            lblPerformance.TabIndex = 5;
+            lblPerformance.Text = "label1";
+            // 
+            // btnOpen
+            // 
+            btnOpen.BackColor = Color.MintCream;
+            btnOpen.Cursor = Cursors.Hand;
+            btnOpen.FlatStyle = FlatStyle.Popup;
+            btnOpen.Font = new Font("Montserrat Medium", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnOpen.Location = new Point(897, 492);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new Size(134, 42);
+            btnOpen.TabIndex = 6;
+            btnOpen.Text = "Show";
+            btnOpen.UseVisualStyleBackColor = false;
+            btnOpen.Click += btnOpen_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSteelBlue;
+            ClientSize = new Size(1134, 549);
+            Controls.Add(btnOpen);
+            Controls.Add(lblPerformance);
+            Controls.Add(txtPrompt);
+            Controls.Add(btnExecute);
+            Controls.Add(btnGenerate);
+            Controls.Add(rtbRun);
+            Controls.Add(rtbOutput);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "C# Generator";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox rtbOutput;
+        private RichTextBox rtbRun;
+        private Button btnGenerate;
+        private Button btnExecute;
+        private TextBox txtPrompt;
+        private Label lblPerformance;
+        private Button btnOpen;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,344 @@
+﻿using System.Data;
+using System.Text;
+using Newtonsoft.Json;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis;
+using System.Text.RegularExpressions;   
+using System.IO;
+
+namespace GeminiC__App
+{
+    public partial class Form1 : Form
+    {
+        /*
+        private const string ApiKey = "AIzaSyChVB52MO6q_GWG9Po4CCOq7YZPN_UlaTE";
+        private const string GeminiApiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + ApiKey;
+        */
+
+        private const string ApiKey = "AIzaSyCGuJvxNXXMrdOUXCp4l6LZwtT5p_nkkUs";
+        private const string GeminiApiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + ApiKey;
+        
+
+        private string scriptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GeneratedScript.csx"); // Application directory
+        // Autosize Prep
+        private Size originalFormSize;
+        private Rectangle originalbtnGenerate;
+        private Rectangle originalbtnExecute;
+        private Rectangle originalbtnShow;
+        private Rectangle originalrtbOutput;
+        private Rectangle originaltxtPrompt;
+        private Rectangle originalrtbRun;
+        private Rectangle originallblPerformance;
+
+        private float originalGenerateFont;
+        private float originalExecuteFont;
+        private float originalShowFont;
+        private float originalLabelFont;
+
+        public Form1()
+        {
+            InitializeComponent();  // Things are displayed on the form y default
+            lblPerformance.Text = "";
+            rtbOutput.Text = " Welcome user, please input a Prompt!\n" +
+                             "------------------------------------------------------------------------------------------------\n " +
+                             "Default: The generated C# program will not use Class or Method. Please manually request if needed.\n" +
+                             "------------------------------------------------------------------------------------------------\n " +
+                             "Generate: call Gemini\n " +
+                             "Execute: run the script\n " +
+                             "Show: display existing file content";
+            // Saving the element's orginal size
+            originalFormSize = this.Size;
+            originalbtnGenerate = new Rectangle(btnGenerate.Location, btnGenerate.Size);
+            originalGenerateFont = btnGenerate.Font.Size;
+            originalbtnExecute = new Rectangle(btnExecute.Location, btnExecute.Size);
+            originalExecuteFont = btnExecute.Font.Size;
+            originalbtnShow = new Rectangle(btnOpen.Location, btnOpen.Size);
+            originalShowFont = btnOpen.Font.Size;
+            originaltxtPrompt = new Rectangle(txtPrompt.Location, txtPrompt.Size);
+            originalrtbOutput = new Rectangle(rtbOutput.Location, rtbOutput.Size);
+            originalrtbRun = new Rectangle(rtbRun.Location, rtbRun.Size);
+            originallblPerformance = new Rectangle(lblPerformance.Location, lblPerformance.Size);
+            originalLabelFont = lblPerformance.Font.Size;
+
+            this.Resize += Form1_Resize;
+
+        }
+        // Resize the form
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ResizeControl(btnGenerate, originalbtnGenerate, originalGenerateFont);
+            ResizeControl(btnExecute, originalbtnExecute, originalExecuteFont);
+            ResizeControl(btnOpen, originalbtnShow, originalShowFont);
+            ResizeControl(txtPrompt, originaltxtPrompt);
+            ResizeControl(rtbOutput, originalrtbOutput);
+            ResizeControl(rtbRun, originalrtbRun);
+            ResizeControl(lblPerformance, originallblPerformance, originalLabelFont);
+        }
+        // Resize the control
+        private void ResizeControl(Control control, Rectangle originalRect, float? originalFontSize = null)
+        {
+            float xRatio = (float)this.Width / originalFormSize.Width;
+            float yRatio = (float)this.Height / originalFormSize.Height;
+
+            int newX = (int)(originalRect.X * xRatio);
+            int newY = (int)(originalRect.Y * yRatio);
+            int newWidth = (int)(originalRect.Width * xRatio);
+            int newHeight = (int)(originalRect.Height * yRatio);
+
+            control.Location = new Point(newX, newY);
+            control.Size = new Size(newWidth, newHeight);
+
+            if (originalFontSize.HasValue)
+            {
+                float newFontSize = Math.Min(xRatio, yRatio) * originalFontSize.Value;
+                control.Font = new Font(control.Font.FontFamily, newFontSize, control.Font.Style);
+            }
+        }
+
+        // Generate button
+        private async void btnGenerate_Click(object sender, EventArgs e)
+        {
+            lblPerformance.ForeColor = Color.Black;
+            if (string.IsNullOrWhiteSpace(txtPrompt.Text))
+            {
+                MessageBox.Show("Please enter a prompt.");
+                return;
+            }
+
+            btnGenerate.Enabled = false;
+            btnExecute.Enabled = false;
+            btnOpen.Enabled = false;
+            lblPerformance.Text = "Generating.....";
+
+            string promptHolder = txtPrompt.Text;
+            string promptReady;
+            if (promptHolder.Contains("class") || promptHolder.Contains("method"))
+            {
+                promptReady = promptHolder;
+            }
+            else
+            {
+                promptReady = promptHolder + " Do not use any Class or Method in the program.";
+            }
+
+            var startTime = DateTime.Now;
+            var scriptContent = await GenerateScriptFromGemini(promptReady); // Input prompt
+            var endTime = DateTime.Now;
+
+            if (!string.IsNullOrEmpty(scriptContent))
+            {
+                SaveScriptToFile(scriptContent);
+                // API response time
+                lblPerformance.Text = $"API Response Time: {(endTime - startTime).TotalSeconds:F2} seconds";
+                lblPerformance.ForeColor = Color.Green;
+            }
+            btnGenerate.Enabled = true;
+            btnExecute.Enabled = true;
+            btnOpen.Enabled = true;
+        }
+
+        // Execute button
+        private async void btnExecute_Click_1(object sender, EventArgs e)
+        {
+            lblPerformance.ForeColor = Color.Black;
+            btnGenerate.Enabled = false;
+            btnExecute.Enabled = false;
+            btnOpen.Enabled = false;
+            lblPerformance.Text = "Executing.....";
+            string result = await ExecuteScript();
+            rtbRun.Text = result ?? "No result returned from script execution.";
+            lblPerformance.Text = "Execution completed.";
+            lblPerformance.ForeColor = Color.Green;
+            btnGenerate.Enabled = true;
+            btnExecute.Enabled = true;
+            btnOpen.Enabled = true;
+        }
+
+        // Show button
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            lblPerformance.ForeColor = Color.Black;
+            btnGenerate.Enabled = false;
+            btnExecute.Enabled = false;
+            btnOpen.Enabled = false;
+            lblPerformance.Text = "Opening...";
+
+            if (File.Exists(scriptPath))
+            {
+                rtbOutput.Text = "The last program was created:\n------------------------------------\n" + File.ReadAllText(scriptPath);
+            }
+            else
+            {
+                rtbOutput.Text = "Script file not found.";
+                rtbOutput.ForeColor = Color.Red;
+            }
+            lblPerformance.Text = "Open complete";
+            lblPerformance.ForeColor = Color.Green;
+            btnGenerate.Enabled = true;
+            btnExecute.Enabled = true;
+            btnOpen.Enabled = true;
+        }
+
+        // Call Gemini API to generate C# script
+        private async Task<string> GenerateScriptFromGemini(string prompt)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var jsonBody = $@"{{
+                        ""contents"": [
+                            {{
+                                ""role"": ""user"",
+                                ""parts"": [
+                                    {{
+                                        ""text"": ""Generate a C# script (.csx) that {prompt}.Include all necessary 'using' statements (e.g., 'using System;') at the top of the code.""
+                                    }}
+                                ]
+                            }}
+                        ],
+                        ""generationConfig"": {{
+                            ""temperature"": 0.7,
+                            ""maxOutputTokens"": 4096
+                        }}
+                    }}";
+
+                    var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+                    var response = await client.PostAsync(GeminiApiUrl, content);
+                    response.EnsureSuccessStatusCode();
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    var jsonResponse = JsonConvert.DeserializeObject<dynamic>(responseBody);
+                    string rawScript = jsonResponse.candidates[0].content.parts[0].text.ToString();
+                    rawScript = rawScript.Replace("\\n", Environment.NewLine).Replace("\\\"", "\"");
+                    return ExtractCSharpCode(rawScript); // Single extraction step
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error calling Gemini API: {ex.Message}");
+                return null;
+            }
+        }
+
+        // Extract C# code from the raw Gemini responsec
+        private string ExtractCSharpCode(string rawResponse)
+        {
+            try
+            {
+                // Match code block between ```csharp or ``` markers
+                var match = Regex.Match(rawResponse, @"```(?:csharp)?\s*([\s\S]*?)\s*```");
+                if (match.Success)
+                {
+                    string code = match.Groups[1].Value.Trim();
+                    if (!string.IsNullOrWhiteSpace(code))
+                    {
+                        return code; // Return the raw C# code as-is
+                    }
+                }
+                return rawResponse.Trim();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error extracting code: {ex.Message}");
+                return rawResponse; // Fallback to raw response
+            }
+
+        }
+
+        // Save generated script to file
+        private void SaveScriptToFile(string scriptContent)
+        {
+            try
+            {
+                File.WriteAllText(scriptPath, scriptContent);
+                // Display the saved script content in the output box
+                rtbOutput.Text = "Script saved to: " + scriptPath + "\n\n" + scriptContent;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving script: {ex.Message}");
+            }
+        }
+
+        // Execute script using Roslyn
+        private async Task<string> ExecuteScript()
+        {
+            try
+            {
+                if (!File.Exists(scriptPath))
+                {
+                    return "Script file not found.";
+                }
+
+                var scriptOptions = ScriptOptions.Default
+                                    .WithReferences(AppDomain.CurrentDomain.GetAssemblies()
+                                    .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
+                                    .Select(a => MetadataReference.CreateFromFile(a.Location)))
+                                    .WithImports("System.Math", 
+                                                 "System.Linq",
+                                                 "System.Numerics",
+                                                 "System.Collections.Generic");
+                string scriptContent = await File.ReadAllTextAsync(scriptPath);
+                var originalConsoleOut = Console.Out;
+                using var consoleOutput = new StringWriter();
+                Console.SetOut(consoleOutput); // Redirect console output to capture it
+
+                try
+                {
+                    var resultState = await CSharpScript.RunAsync(scriptContent, scriptOptions);
+
+                    // Capture console output and return value
+
+                    consoleOutput.Flush(); // Force flush to capture all output
+                    string consoleOutputText = consoleOutput.ToString(); // Get captured console output
+
+                    string result = resultState?.ReturnValue?.ToString() ?? "null";
+
+                    // Combine output and return value
+                    string output = "Execution successful.";
+                    if (!string.IsNullOrEmpty(consoleOutputText))
+                    {
+                        return output += $"\nOutput:\n{consoleOutputText}";
+                    }
+                    if (result != "null")
+                    {
+                        return output += $"\nReturn Value: {result}";
+                    }
+                    return output + "\nNo output or value return\n" +
+                        "void method had been used !";
+                }
+
+                catch (CompilationErrorException ex)
+                {
+                    return $"Compilation Error: {string.Join("\n", ex.Diagnostics)}";
+                }
+
+                catch (Exception ex)
+                {
+                    return $"Execution executing script: {ex.Message}";
+                }
+                finally
+                {
+                    Console.SetOut(originalConsoleOut); // Restore original console output
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return $"Execution Error: {ex.Message}";
+            }
+
+        }
+
+        private void txtPrompt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
